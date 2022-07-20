68a0cb1f0d1f04b42bd02dd2408456f958dd208d
Emile // Tundersz <emiledavidson395@gmail.com>
2022-07-20 15:47:52 +0200

    updated unity version + added method extension + added HDRP

--- Assets/Toolbox/Optional/TweenMachine/Editor/TweenBuildPropertyDrawer.cs
+++ Assets/Toolbox/Optional/TweenMachine/Editor/TweenBuildPropertyDrawer.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.EnterpriseServices;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using Toolbox.Required;
 using UnityEditor;
 using UnityEngine;
